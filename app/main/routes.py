58238e95037c012ac4a1fdc43dce4f1015976d09
mutembeijoe <mutembeijoe@gmail.com>
2019-10-30 15:56:30 +0300

    intergrates and reorganizes changes pulled from upstream main project

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,12 +1,8 @@
 from flask import Blueprint,render_template
-from app.requests import get_all_countries
-
 
 
 main = Blueprint('main',__name__)
 
-
 @main.route('/')
 def home():
-    all_countries = get_all_countries()
     return render_template('home.html')
